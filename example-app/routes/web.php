99a130df3dfbabce4b7e2e78085b533a5b3ff1f4
AlVio21 <160828042+AlVio21@users.noreply.github.com>
2024-06-11 17:00:27 +0700

    11 Juni 2024

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -3,43 +3,29 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\ProdiController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function() {
-    return "Halaman About";
-});
-
-Route::get('profil', function() {
-    return view('profil');
-});
-
-//Route dengan parameter
-Route::get('welcome/{salam}', function ($salam) {
-    // return 'Selamat '. $salam;
-    return view('salam')->with ('viewsalam', $salam);
-});
-
-// Route Tanpa parameter
-// Terdapat array $list
-Route::get('listdata', function () {
-    $list = ["Sistem Informasi", "Informatika", "Manajemen"];
-    $listmhs = [
-        ["npm" => "001", "nama" => "holan"],
-        ["npm" => "002", "nama" => "viola"]
-    ];
-    return view('listprodi')
-        ->with('viewlist', $list)
-        ->with('viewmhs', $listmhs);
-});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('fakultas',FakultasController::class);
 Route::resource('prodi', ProdiController::class);
 Route::resource('mahasiswa', MahasiswaController::class);
 
 
-Route::get('dashboard', [DashboardController::class, 'index'] );
+Route::get('dashboard', [DashboardController::class, 'index'] )->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
